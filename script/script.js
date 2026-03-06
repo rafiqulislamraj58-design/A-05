95617965ca2f75b297891d8edc1bfa4145b22dc2
rafiqulislamraj58-design <rafiqulislamraj58@gmail.com>
2026-03-07 00:45:23 +0600

    log interface  script and home

---
+++ script/script.js
@@ -0,0 +1,18 @@
+const userInput = document.querySelector('#user');
+const passInput = document.querySelector('#Password');
+const logIn = document.getElementById('submitBtn')
+document.getElementById('submitBtn').addEventListener('click',function(){
+   if (userInput.value==='admin' && passInput.value==='admin123') {
+    logIn.innerText = 'Logged In';
+    setTimeout(function () {
+        
+        window.location.replace('home.html');
+        
+    },2000)
+    alert('log in success');
+    
+    
+   } else {
+    alert('invaild user or password');
+   }
+});
